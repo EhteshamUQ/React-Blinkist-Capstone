5d1013944f58f51368345abbeeec464183ec31e3
Ehtesham <ehteshamuddinqureshi.mohd@zemosolabs.com>
2022-01-11 15:56:24 +0530

    Icon Title test added

---
+++ src/components/molecules/IconTitle/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import IconWithText from "../../atoms/iconWIthText";
+import PersonIcon from "../../icons/personIcon";
+
+test('should display icon with title', () => {
+    render(<IconWithText children={PersonIcon} title="34k reads"/>);
+    const text = screen.getByText(/34k reads/i);
+    const icon = screen.getByTestId('personicon')
+})
